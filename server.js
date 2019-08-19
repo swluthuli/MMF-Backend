106024f536893b5b90171fe89613a909d703be0e
swluthuli <swluthuli@gmail.com>
2019-08-19 19:57:43 +0200

    main route

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.use((req, res, next) => {
     next();
   });
   app.get('/favicon.ico', (req, res) => res.status(204));
-  app.get('/', (req, res) => res.status(200));
+  app.get('/', (req, res) => res.send("this are my routes"));
 
 // api routes
 app.use('/users', require('./users/users.controller'));
